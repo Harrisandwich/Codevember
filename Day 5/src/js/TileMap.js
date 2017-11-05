d7bc960658d250300976c41ba1f1a20e7010e0e6
Harrison Hutcheon <harrison.hutcheon@threepointturn.com>
2017-11-05 15:22:09 +0100

    tile css

---
+++ Day 5/src/js/TileMap.js
@@ -0,0 +1,32 @@
+var TileMap = [{
+	name: "CaveEntrance-1",
+	passable: true
+},
+{
+	name: "CaveInterier-1",
+	passable: false
+},
+{
+	name: "CaveInterier-2",
+	passable: false
+},
+{
+	name: "OverworldGround-1",
+	passable: true
+},
+{
+	name: "OverworldWall-1",
+	passable: false
+}, 
+{
+	name: "OverworldWall-2",
+	passable: false
+}, 
+{
+	name: "OverworldWall-3",
+	passable: false
+},
+{
+	name: "OverworldWall-4",
+	passable: false
+}];
