bd0179e0c7d68bc66b8aef2ed00c0d642e76690c
Harrison Hutcheon <harrison.hutcheon@threepointturn.com>
2017-11-05 14:55:25 +0100

    Day 5 images and code

---
+++ Day 5/images/src/js/TileMap.js
@@ -0,0 +1,32 @@
+var TileMap = [{
+	fileName: "CaveEntrance-1.png",
+	passable: true
+},
+{
+	fileName: "CaveInterier-1.png",
+	passable: false
+},
+{
+	fileName: "CaveInterier-2.png",
+	passable: false
+},
+{
+	fileName: "OverworldGround-1.png",
+	passable: true
+},
+{
+	fileName: "OverworldWall-1.png",
+	passable: false
+}, 
+{
+	fileName: "OverworldWall-2.png",
+	passable: false
+}, 
+{
+	fileName: "OverworldWall-3.png",
+	passable: false
+},
+{
+	fileName: "OverworldWall-4.png",
+	passable: false
+}];
